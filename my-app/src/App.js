a268400eb9d7ed85fba3adb3f4d241759b2d8494
krishna <manoharmeena242@gmail.com>
2023-04-19 23:51:03 +0530

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+import Counter from './components/Counter';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Greet/>
+    <Counter/>
+    </div>
+  );
+}
+
+export default App;
